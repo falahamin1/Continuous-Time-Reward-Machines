df46eea740601b3917f02f3e7e122bf8cf647cd6
Amin Falah <falahamin@gmail.com>
2024-09-23 11:07:02 -0600

    Added a shell script which executes all three models

---
+++ comparison-three-models.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --time=2:00:00                # Adjust the time as needed (e.g., 2 hours)
+#SBATCH --partition=aa100              # Set the partition to aa100
+#SBATCH --ntasks=1                     # Number of tasks (usually 1 for Python scripts)
+#SBATCH --cpus-per-task=1           # Number of CPU cores for the task
+#SBATCH --gres=gpu:1                 # Request 1 GPU
+#SBATCH --job-name=python-comparison   # Job name
+#SBATCH --output=python-comparison.%j.out  # Output file (%j will be replaced by job ID)
+
+# Load Anaconda module
+module purge
+module load anaconda
+
+# Activate the Conda environment
+conda activate amfa-custom-env
+
+# Run the Python script with the given arguments
+python3 Comparison.py --env firefighter-car --specify_dimension yes --rows 7 --columns 7 --runs 30 --threshold 0.9 --max_episodes 100000 --save_file firefighter-comparison-server --buffer_size 50000 &
+
+python3 Comparison.py --env treasure-map --specify_dimension yes --rows 7 --columns 7 --runs 30 --threshold 0.9 --max_episodes 700000 --save_file treasurehunt-comparison-server --buffer_size 100000 &
+
+python3 Comparison.py --env cop-car --specify_dimension yes --rows 7 --columns 7 --runs 30 --threshold 0.9 --max_episodes 250000 --save_file cop-car-comparison-server --buffer_size 75000 &
+
+wait
+
+
+
