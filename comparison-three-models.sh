86e060531571457b0ed8abd8efc42e17a7664aed
Amin Falah <falahamin@gmail.com>
2024-09-23 11:15:01 -0600

    Added more time for the shell script

--- comparison-three-models.sh
+++ comparison-three-models.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --time=2:00:00                # Adjust the time as needed (e.g., 2 hours)
+#SBATCH --time=4:00:00                # Adjust the time as needed (e.g., 2 hours)
 #SBATCH --partition=aa100              # Set the partition to aa100
 #SBATCH --ntasks=1                     # Number of tasks (usually 1 for Python scripts)
 #SBATCH --cpus-per-task=1           # Number of CPU cores for the task
