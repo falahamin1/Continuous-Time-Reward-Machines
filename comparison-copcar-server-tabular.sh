1374c0bf5359acf3ed9221ef9575cd6bcc067440
Amin Falah <falahamin@gmail.com>
2024-10-23 07:49:53 -0600

    fixed a bug

--- comparison-copcar-server-tabular.sh
+++ comparison-copcar-server-tabular.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --time= 0:15:00                # Adjust the time as needed (e.g., 2 hours)
+#SBATCH --time= 00:15:00                # Adjust the time as needed (e.g., 2 hours)
 #SBATCH --partition=amilan            # Set the partition to aa100
 #SBATCH --ntasks=1                     # Number of tasks (usually 1 for Python scripts)
 #SBATCH --cpus-per-task=1           # Number of CPU cores for the task
